ad181cb2f0c4515ef62f2f558df85d03c1c69903
Fabian Linzberger <e@lefant.net>
2016-06-18 16:17:03 +0200

    fix version in start script

--- kgsgtp/kgsGtp.sh
+++ kgsgtp/kgsGtp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-java -jar kgsGtp-3.5.11/kgsGtp.jar kgsGtp.conf name=$NAME password=$PASSWORD "${KGSGTP_ARGS}" "$@"
+java -jar kgsGtp-3.5.20/kgsGtp.jar kgsGtp.conf name=$NAME password=$PASSWORD "${KGSGTP_ARGS}" "$@"
