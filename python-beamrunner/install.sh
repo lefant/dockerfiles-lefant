4e0df152449bfedcc4bc6b243f60e9f50e47ef61
Fabian Linzberger <e@lefant.net>
2018-02-24 09:54:33 +0100

    more minimal python-beamrunner

--- python-beamrunner/install.sh
+++ python-beamrunner/install.sh
@@ -16,33 +16,10 @@ mg
 wget
 
 python-dev
-python-opencv
 python-setuptools
 python-snappy
-python-tk
 python-wheel
 cython
-
-cmake
-gfortran
-graphviz
-libagg-dev
-libatlas-dev
-libffi-dev
-libfreetype6-dev
-libhdf5-dev
-libjpeg-dev
-liblapack-dev
-libncurses5-dev
-libopenblas-dev
-libpng12-dev
-libxft-dev
-libxml2-dev
-libxslt-dev
-swig
-zlib1g-dev
-
-ttf-bitstream-vera
 EOF
 )
 
--- python-beamrunner/run_jupyter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-jupyter nbextension enable --py widgetsnbextension
-jupyter notebook --ip=0.0.0.0 --allow-root
